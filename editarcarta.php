6adb17a4cd9a42a9ab07d2cfffcbb6275a4fe030
KevinMunoz10 <kevin.munoz@campusucc.edu.co>
2023-10-29 16:24:52 -0500

    BibliotecaV3

--- editarcarta.php
+++ editarcarta.php
@@ -1,47 +1,53 @@
-<?php 
-	require 'partials/header.php';
-	require 'database.php';
+<?php
+require 'partials/header.php'; // Incluye el encabezado de la página.
+require 'database.php'; // Incluye el archivo de configuración de la base de datos.
 
-	$carta = $_GET['id'];
+$carta = $_GET['id']; // Obtiene el valor del parámetro 'id' de la URL.
 
-	$records = $conn->prepare('SELECT idcarta, NombreSolicitante, cedula, fecha, razonPrestamo FROM cartasolicitud WHERE idcarta = :idcarta');
-	$records->bindParam(':idcarta', $carta);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+$records = $conn->prepare('SELECT idcarta, NombreSolicitante, cedula, fecha, razonPrestamo FROM cartasolicitud WHERE idcarta = :idcarta');
+$records->bindParam(':idcarta', $carta); // Vincula el valor de 'idcarta' al parámetro en la consulta SQL.
+$records->execute(); // Ejecuta la consulta para obtener los detalles de la carta.
+$results = $records->fetch(PDO::FETCH_ASSOC); // Obtiene los resultados como un arreglo asociativo.
 
-	$message = '';
+$message = '';
 
-	if(!count($results) > 0){
-		$message = 'Hubo un problema en el codigo';
-	}
+if (!count($results) > 0) {
+	$message = 'Hubo un problema en el código'; // Verifica si no se encontraron resultados y muestra un mensaje de error.
+}
 ?>
-	
-	<?php if(!empty($message)): ?>
-	    <p> <?= $message ?></p>
-	<?php endif; ?>
 
-	<form class="contform" method="POST" action="procesocarta.php">
-		<h2 class="h2">Editar Carta de Solicitud</h2>
-		<div class="form-group">
-			<label class="control-label">Nombre del Solicitante:</label>
-			<input type="text" class="form-control" name="nombreSolicitante" placeholder="Escribe tu Nombre: " value="<?= $results['NombreSolicitante']?>">
-		</div>
-		<div class="form-group">
-			<label class="control-label">Cedula:</label>
-			<input type="number" class="form-control" name="cedula" placeholder="Escribe tu Cedula: " value="<?= $results['cedula']?>">
-		</div>
-		<div class="form-group">
-			<label class="control-label">Fecha:</label>
-			<input type="date" name="fecha" placeholder="Introduzca la fecha" value="<?= $results['fecha']?>">
-		</div>
-		<div class="form-group">
-			<label class="control-label">Razon del Prestamo:</label>
-			<textarea class="form-control" placeholder="razon:" name="razonPrestamo"><?= $results['razonPrestamo']?></textarea>
-		</div>
-		<input type="hidden" name="idcarta" value="<?= $results['idcarta']?>">
-		<div class="form-group">
-			<button type="submit" class="btn btn-primary">Modificar</button>
-		</div>
-	</form>
+<?php if (!empty($message)): ?>
+	<p>
+		<?= $message ?>
+	</p> <!-- Muestra el mensaje de error. -->
+<?php endif; ?>
 
-<?php require 'partials/footer.php' ?>
+<form class="contform" method="POST" action="procesocarta.php">
+	<h2 class="h2">Editar Carta de Solicitud</h2>
+	<div class="form-group">
+		<label class="control-label">Nombre del Solicitante:</label>
+		<input type="text" class="form-control" name="nombreSolicitante" placeholder="Escribe tu Nombre: "
+			value="<?= $results['NombreSolicitante'] ?>">
+	</div>
+	<div class="form-group">
+		<label class="control-label">Cedula:</label>
+		<input type="number" class="form-control" name="cedula" placeholder="Escribe tu Cedula: "
+			value="<?= $results['cedula'] ?>">
+	</div>
+	<div class="form-group">
+		<label class="control-label">Fecha:</label>
+		<input type="date" name="fecha" placeholder="Introduzca la fecha" value="<?= $results['fecha'] ?>">
+	</div>
+	<div class="form-group">
+		<label class="control-label">Razon del Prestamo:</label>
+		<textarea class="form-control" placeholder="razon:"
+			name="razonPrestamo"><?= $results['razonPrestamo'] ?></textarea>
+	</div>
+	<input type="hidden" name="idcarta" value="<?= $results['idcarta'] ?>">
+	<!-- Campo oculto para enviar el ID de la carta. -->
+	<div class="form-group">
+		<button type="submit" class="btn btn-primary">Modificar</button>
+	</div>
+</form>
+
+<?php require 'partials/footer.php'; // Incluye el pie de página. ?>
--- editardewey.php
+++ editardewey.php
@@ -1,35 +1,39 @@
-<?php 
-	require 'partials/header.php';
-	require 'database.php';
+<?php
+require 'partials/header.php'; // Incluye el encabezado de la página.
+require 'database.php'; // Incluye el archivo de configuración de la base de datos.
 
-	$carta = $_GET['id'];
+$carta = $_GET['id']; // Obtiene el valor del parámetro 'id' de la URL.
 
-	$records = $conn->prepare('SELECT idcdd, nombreDewey FROM cdd WHERE idcdd = :idcdd');
-	$records->bindParam(':idcdd', $carta);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+$records = $conn->prepare('SELECT idcdd, nombreDewey FROM cdd WHERE idcdd = :idcdd');
+$records->bindParam(':idcdd', $carta); // Vincula el valor de 'idcdd' al parámetro en la consulta SQL.
+$records->execute(); // Ejecuta la consulta para obtener los detalles de la categoría Dewey.
+$results = $records->fetch(PDO::FETCH_ASSOC); // Obtiene los resultados como un arreglo asociativo.
 
-	$message = '';
+$message = '';
 
-	if(!count($results) > 0){
-		$message = 'Hubo un problema en el codigo';
-	}
+if (!count($results) > 0) {
+	$message = 'Hubo un problema en el código'; // Verifica si no se encontraron resultados y muestra un mensaje de error.
+}
 ?>
-	
-	<?php if(!empty($message)): ?>
-	    <p> <?= $message ?></p>
-	<?php endif; ?>
 
-	<form class="contform" method="POST" action="procesodewey.php">
-		<h2 class="h2">Editar Categoria de Dewey</h2>
-		<div class="form-group">
-			<label class="control-label">Nombre de la Categoria:</label>
-			<input type="text" class="form-control" name="nombreDewey" placeholder="Escribe tu Nombre: " value="<?= $results['nombreDewey']?>">
-		</div>
-		<input type="hidden" name="idcdd" value="<?= $results['idcdd']?>">
-		<div class="form-group">
-			<button type="submit" class="btn btn-primary">Modificar</button>
-		</div>
-	</form>
+<?php if (!empty($message)): ?>
+	<p>
+		<?= $message ?>
+	</p> <!-- Muestra el mensaje de error. -->
+<?php endif; ?>
 
-<?php require 'partials/footer.php' ?>
+<form class="contform" method="POST" action="procesodewey.php">
+	<h2 class="h2">Editar Categoria de Dewey</h2>
+	<div class="form-group">
+		<label class="control-label">Nombre de la Categoria:</label>
+		<input type="text" class="form-control" name="nombreDewey" placeholder="Escribe la Categoría: "
+			value="<?= $results['nombreDewey'] ?>">
+	</div>
+	<input type="hidden" name="idcdd" value="<?= $results['idcdd'] ?>">
+	<!-- Campo oculto para enviar el ID de la categoría Dewey. -->
+	<div class="form-group">
+		<button type="submit" class="btn btn-primary">Modificar</button>
+	</div>
+</form>
+
+<?php require 'partials/footer.php'; // Incluye el pie de página. ?>
