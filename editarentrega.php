4e0c54f80fdd6094fd87d688230469b2e7fb0091
KevinMunoz10 <kevin.munoz@campusucc.edu.co>
2023-10-29 16:39:53 -0500

    BibliotecaV4

--- editarentrega.php
+++ editarentrega.php
@@ -1,79 +1,39 @@
-<?php 
-	require 'partials/header.php';
-	require 'database.php';
+<?php
+require 'partials/header.php'; // Incluye el encabezado de la página.
+require 'database.php'; // Incluye el archivo de configuración de la base de datos.
 
-	$carta = $_GET['id'];
+$carta = $_GET['id']; // Obtiene el valor del parámetro 'id' de la URL.
 
-	$records = $conn->prepare('SELECT identrega, usuarioId, libroId, multaId, fechaEntrega FROM entrega WHERE identrega = :identrega');
-	$records->bindParam(':identrega', $carta);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+$records = $conn->prepare('SELECT idcdd, nombreDewey FROM cdd WHERE idcdd = :idcdd');
+$records->bindParam(':idcdd', $carta); // Vincula el valor de 'idcdd' al parámetro en la consulta SQL.
+$records->execute(); // Ejecuta la consulta para obtener los detalles de la categoría Dewey.
+$results = $records->fetch(PDO::FETCH_ASSOC); // Obtiene los resultados como un arreglo asociativo.
 
-	$message = '';
+$message = '';
 
-	if(!count($results) > 0){
-		$message = 'Hubo un problema en el codigo';
-	}
-
-	#consultar usuarios
-	$recordsusu = $conn->prepare('SELECT idusuario, nombreUsuario FROM usuario');
-
-	#consultar libros
-	$recordslib = $conn->prepare('SELECT idlibro, nombreLibro FROM libro');
-
-	#consultar Multa
-	$recordsmul = $conn->prepare('SELECT idmulta, tipoMulta FROM multa');
-
-	$recordsusu->execute();
-	$recordslib->execute();
-	$recordsmul->execute();
+if (!count($results) > 0) {
+	$message = 'Hubo un problema en el código'; // Verifica si no se encontraron resultados y muestra un mensaje de error.
+}
 ?>
-	
-	<?php if(!empty($message)): ?>
-	    <p> <?= $message ?></p>
-	<?php endif; ?>
 
-	<form class="contform" method="POST" action="procesoentrega.php">
-		<h2 class="h2">Editar Entrega</h2>
-		
-		<div class="form-group">
-			<label class="control-label">Usuarios:</label>
-			<select class="form-select" name="usuarioId">
-				<?php if ($recordsusu->rowcount() > 0): ?>
-				<?php for ($i=0; $i < $resultsusu = $recordsusu->fetch(PDO::FETCH_ASSOC); $i++): ?>
-					<option value="<?= $resultsusu['idusuario'] ?>"><?= $resultsusu['nombreUsuario'] ?></option>
-				<?php endfor; ?>
-				<?php endif; ?>
-			</select>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Libros:</label>
-			<select class="form-select" name="libroId">
-				<?php if ($recordslib->rowcount() > 0): ?>
-				<?php for ($i=0; $i < $resultslib = $recordslib->fetch(PDO::FETCH_ASSOC); $i++): ?>
-					<option value="<?= $resultslib['idlibro'] ?>"><?= $resultslib['nombreLibro'] ?></option>
-				<?php endfor; ?>
-				<?php endif; ?>
-			</select>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Multas:</label>
-			<select class="form-select" name="multaId">
-				<?php if ($recordsmul->rowcount() > 0): ?>
-				<?php for ($i=0; $i < $resultsmul = $recordsmul->fetch(PDO::FETCH_ASSOC); $i++): ?>
-					<option value="<?= $resultsmul['idmulta'] ?>"><?= $resultsmul['tipoMulta'] ?></option>
-				<?php endfor; ?>
-				<?php endif; ?>
-			</select>
-		</div>
-		<div class="form-group">
-			<label class="control-label">Fecha de Entrega:</label>
-			<input type="date" class="form-control" name="fechaEntrega" placeholder="FECHA: " value="<?= $results['fechaEntrega'] ?>">
-		</div>
-		<input type="hidden" name="identrega" value="<?= $results['identrega']?>">
-		<div class="form-group">
-			<button type="submit" class="btn btn-primary">Modificar</button>
-		</div>
-	</form>
+<?php if (!empty($message)): ?>
+	<p>
+		<?= $message ?>
+	</p> <!-- Muestra el mensaje de error. -->
+<?php endif; ?>
 
-<?php require 'partials/footer.php' ?>
+<form class="contform" method="POST" action="procesodewey.php">
+	<h2 class="h2">Editar Categoria de Dewey</h2>
+	<div class="form-group">
+		<label class="control-label">Nombre de la Categoria:</label>
+		<input type="text" class="form-control" name="nombreDewey" placeholder="Escribe la Categoría: "
+			value="<?= $results['nombreDewey'] ?>">
+	</div>
+	<input type="hidden" name="idcdd" value="<?= $results['idcdd'] ?>">
+	<!-- Campo oculto para enviar el ID de la categoría Dewey. -->
+	<div class="form-group">
+		<button type="submit" class="btn btn-primary">Modificar</button>
+	</div>
+</form>
+
+<?php require 'partials/footer.php'; // Incluye el pie de página. ?>
